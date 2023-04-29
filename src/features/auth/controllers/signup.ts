7a10df9841a0211acb750e6252e3b0b61c0a695a
yonatan <yonatannasimov@gmail.com>
2023-04-29 15:45:15 +0300

    ok

--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -2,9 +2,10 @@ import { ObjectId } from "mongoose";
 import { Request, Response } from "express";
 import { joiValidation } from "@global/decorators/joi-validation.decorators";
 import { signupSchema } from "@auth/schemes/signup";
-import { IAuthDocument } from '@auth/interfaces/auth.interface';
+import { IAuthDocument, ISignUpData } from '@auth/interfaces/auth.interface';
 import { authService } from "@service/db/auth.service";
 import { BadRequestError } from "@global/helpers/error-handler";
+import { Helpers } from "@global/helpers/helpers";
 
 export class SignUp {
     @joiValidation(signupSchema)
@@ -14,5 +15,30 @@ export class SignUp {
         if (checkIfUserExist) {
             throw new BadRequestError('Invalid credentials');
         }
+
+        const authObjectId: ObjectId = new ObjectId();
+        const userObjectId: ObjectId = new ObjectId();
+        const uId = `${Helpers.generateRandomIntegers(12)}`;
+        const authData: IAuthDocument = SignUp.prototype.signupData({
+            _id: authObjectId,
+            uId,
+            username,
+            email,
+            password,
+            avatarColor
+        });
+    }
+
+    private signupData(data: ISignUpData): IAuthDocument {
+        const { _id, username, email, uId, password, avatarColor } = data;
+        return {
+            _id,
+            uId,
+            username: Helpers.firstLetterToUpperCase(username),
+            email: Helpers.lowerCase(email),
+            password,
+            avatarColor,
+            createdAt: new Date()
+        } as IAuthDocument;
     }
 }
